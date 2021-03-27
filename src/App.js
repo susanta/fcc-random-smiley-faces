1b04b8e13bebd9bdb3e6926464aa8f5522e351ba
Susanta <inbox.susanta@gmail.com>
2021-03-27 17:46:19 +0530

    Smiley face part-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 90;
+const eyeRedious = 50;
+
+const App = () => (
+  <svg width={width} height={height}>
+    <circle
+      cx={centerX}
+      cy={centerY}
+      r={centerY - strokeWidth / 2}
+      fill='yellow'
+      stroke='black'
+      stroke-width={strokeWidth}
+    />
+    <circle
+      cx={centerX - eyeOffsetX}
+      cy={centerY - eyeOffsetY}
+      r={eyeRedious}
+    />
+    <circle
+      cx={centerX + eyeOffsetX}
+      cy={centerY - eyeOffsetY}
+      r={eyeRedious}
+    />
+  </svg>
+);
 
 export default App;
